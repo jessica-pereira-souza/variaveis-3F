89eac0c4c6183a9f0fbfebb812f32ebc8d580e92
jessica-pereira-souza <126929259+jessica-pereira-souza@users.noreply.github.com>
2024-08-12 23:09:32 +0000

    12/08/24 - 2

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-const idade= 19;
+let idade= 19;
 const anoNascimento= 2005;
 let anoAtual= 2024;
+const nomeCompleto= "Jéssica Pereira de Souza";
 
 anoAtual = 2024 + 1;
-
 console.log(anoAtual);
+
+idade = 19 + 1;
+console.log(idade);
+
+console.log("Meu nome é "  + nomeCompleto + " nascida no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos ");
