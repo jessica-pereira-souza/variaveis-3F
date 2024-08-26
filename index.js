ae15bb764f6549b1a9daccf90a9152edde078034
jessica-pereira-souza <126929259+jessica-pereira-souza@users.noreply.github.com>
2024-08-26 23:05:28 +0000

    26.08.2024

--- index.js
+++ index.js
@@ -10,3 +10,21 @@ idade = 19 + 1;
 console.log(idade);
 
 console.log("Meu nome é "  + nomeCompleto + " nascida no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos ");
+
+
+//Agência de viagens Bonito Foz do Iguaçu Tiradentes Urubici Cambará do Sul
+const ListaDeDestinos = new Array(
+  "Bonito",
+  "Foz do Iguaçu",
+  "Tiradentes",
+  "Urubici",
+  "Cambará do Sul"
+); 
+
+console.log(ListaDeDestinos);
+console.log(ListaDeDestinos[4]);
+ListaDeDestinos.push("Coronel Vivida");
+ListaDeDestinos.push("Curitiba");
+ListaDeDestinos.push("Madrid ");
+ListaDeDestinos.push("Berlin");
+console.log(ListaDeDestinos);
