9613e3b35a7a8d33360068867a11ef67e3dc93aa
jessica-pereira-souza <126929259+jessica-pereira-souza@users.noreply.github.com>
2024-09-16 23:06:51 +0000

    16.09.24

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const prompt = require('prompt-sync');
+const entrada = prompt();
+
 let idade= 19;
 const anoNascimento= 2005;
 let anoAtual= 2024;
@@ -12,7 +15,7 @@ console.log(idade);
 console.log("Meu nome é "  + nomeCompleto + " nascida no ano de " + anoNascimento + " e no ano atual " + anoAtual + " completo " + idade + " anos ");
 
 
-//Agência de viagens Bonito Foz do Iguaçu Tiradentes Urubici Cambará do Sul
+//Agência de viagens Bonito, Foz do Iguaçu, Tiradentes, Urubici, Cambará do Sul
 const ListaDeDestinos = new Array(
   "Bonito",
   " Foz do Iguaçu",
@@ -29,12 +32,15 @@ ListaDeDestinos.push(" Madrid");
 ListaDeDestinos.push(" Berlin");
 console.log(ListaDeDestinos);
 
-const idadeComprador = 18;
+var nomeComprador = entrada("Digite o seu nome: ")
+var idadeComprador = entrada("Qual a sua idade? ");
 
 if (idadeComprador >= 18) {
+  console.log("Olá senhor(a) " + nomeComprador);
   console.log("Comprador maior de idade");
   console.log("Destinos disponíveis: " + ListaDeDestinos);
 } else {
+  console.log("Olá senhor(a) " + nomeComprador);
   console.log("Comprador menor de idade");
   console.log("Sentimos muito, a compra não pôde ser finalizada pois você não é maior de idade.");
 }
